e1d7c13c13a192c2c4fb1110e1d911446eb785ba
Aman Dudi <mndudi@gmail.com>
2023-01-23 17:17:36 -0500

    Adding a few comments

--- config.php
+++ config.php
@@ -1,5 +1,9 @@
 <?php
-	$page = $_SERVER['REQUEST_URI'] == '/'||'' ? '/index' : $_SERVER['REQUEST_URI'];
+	/*
+		The following code takes the url param to load the associated file
+		Right now only works for one URL i.e. www.mysite.com/enroll
+	*/
+	$page = $_SERVER['REQUEST_URI'] == '/'||'' ? '/enroll' : $_SERVER['REQUEST_URI'];
 	$uriArr = explode("/",$page);
 	$loadPage = 'pages/page_'.$uriArr[1].'.php';
 
